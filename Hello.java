c9bb1f4c8ea78626c7e4a525ac3d0a85b4d3266c
kilicmurat <72164301+kilicmurat@users.noreply.github.com>
2020-12-14 14:48:43 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello {
         System.out.println("Hello");
         System.out.println("Hello2");
         //System.out.println("Hello3");
+         //System.out.println("Hello3");
     }
 }
