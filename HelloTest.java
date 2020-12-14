c69a754f59c8fad296575879d9f5030f33743265
kilicmurat <72164301+kilicmurat@users.noreply.github.com>
2020-12-14 14:11:53 +0100

    commit1
    ilk dosyalar eklendi

---
+++ HelloTest.java
@@ -0,0 +1,9 @@
+public class HelloTest {
+    public static void main(String[] args) {
+        //test
+        System.out.println("HelloTest");
+        System.out.println("HelloTest1");
+        System.out.println("HelloT2");
+        System.out.println("HelloT3");
+    }
+}
