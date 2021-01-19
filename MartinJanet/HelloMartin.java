8f7965978775c8b4f9f565202440334ecab710d7
kilicmurat <72164301+kilicmurat@users.noreply.github.com>
2021-01-19 11:55:30 +0100

    Commit :1

---
+++ MartinJanet/HelloMartin.java
@@ -0,0 +1,10 @@
+package MartinJanet;
+
+public class HelloMartin {
+    public static void main(String[] args) {
+        /////////
+        System.out.println("Hello");
+        System.out.println("Hello2 Martin");
+
+    }
+}
